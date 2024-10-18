0718af54053bae05009b6c9ae3aeb0d32ede0a0a
voidxin <voidxin@163.com>
2024-10-18 14:53:39 +0800

    测试

---
+++ VXBuyChannel/VXBuyChannel.h
@@ -0,0 +1,18 @@
+//
+//  VXBuyChannel.h
+//  VXBuyChannel
+//
+//  Created by zhangxin on 2024/10/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VXBuyChannel.
+FOUNDATION_EXPORT double VXBuyChannelVersionNumber;
+
+//! Project version string for VXBuyChannel.
+FOUNDATION_EXPORT const unsigned char VXBuyChannelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VXBuyChannel/PublicHeader.h>
+
+
